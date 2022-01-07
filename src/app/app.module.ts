ff727d0237a34b262c2e2c6f233eb50a909148a6
NeoGlanding <neoglanding@protonmail.com>
2022-01-07 20:57:17 +0700

    auto commit without message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { TestComponent } from './test/test.component';
-import { InlineComponent } from './inline/inline.component'
+import { InlineComponent } from './inline/inline.component';
+import { DatabindingComponent } from './databinding/databinding.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     TestComponent,
-    InlineComponent
+    InlineComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule
